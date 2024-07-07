37e3e293b2b275f0a6b8c1c4d2042038a3712b1f
ASAD18-AJ <123192655+ASAD18-AJ@users.noreply.github.com>
2024-07-07 19:53:56 +0530

    navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from '@/components/Header'
 import Providers from "./Providers";
+import Navbar from "@/components/Navbar";
 
 
 
@@ -17,8 +18,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Providers>
-         <Header />
+         <Header />  
+           <Navbar />
          {children}
+        
         </Providers>
         </body>
     </html>
