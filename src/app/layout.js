d560d874447b2d2d00eff29d3b90292989240adf
ASAD18-AJ <123192655+ASAD18-AJ@users.noreply.github.com>
2024-07-07 18:28:34 +0530

    add dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from '@/components/Header'
+import Providers from "./Providers";
 
 
 
@@ -15,8 +16,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
+        <Providers>
+         <Header />
+         {children}
+        </Providers>
         </body>
     </html>
   );
